e28e66fc27ca9860e2e47fdf429bb07bf3d0467c
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-03 13:31:05 +0700

    Create komentar.dart

---
+++ komentar.dart
@@ -0,0 +1,12 @@
+/// ini adalah documentation
+/// yang biasanya akakn di digenerate menjadi dart doc
+void main(){
+    // this is variabel
+    var name='Muhammad Rafiqi';
+    
+    /**
+     *ini adalah komentar
+     * *lebih dari satu baris
+     */
+      print(name);
+}
